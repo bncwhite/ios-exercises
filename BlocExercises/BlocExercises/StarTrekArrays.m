1833443ecad35153fb06b1c2986e3b5d86a0842c
bncwhite <brad@codebybrad.com>
2014-10-10 13:09:50 -0500

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,21 +12,46 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+    NSArray *starTrekCharactersArray = [characterString componentsSeparatedByString:@";"];
+    
+    return starTrekCharactersArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSString *starTrekCharactersString = [characterArray componentsJoinedByString:@";"];
+    
+    return starTrekCharactersString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *characterArraySorted = [characterArray mutableCopy];
+    
+    NSSortDescriptor *sortedStarTrekCharacters = [[NSSortDescriptor alloc] initWithKey:nil ascending:true selector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    [characterArraySorted sortUsingDescriptors:@[sortedStarTrekCharacters]];
+    
+    return characterArraySorted;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    
+    NSMutableArray *worfMayBeAWOL = [characterArray mutableCopy];
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    
+    [worfMayBeAWOL filterUsingPredicate:containsWorf];
+    
+    if (worfMayBeAWOL.count > 0) {
+        return YES;
+    }
+    
+    
     return NO;
 }
 
