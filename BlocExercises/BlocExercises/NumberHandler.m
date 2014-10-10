0de05348118e45e4b8bd348d92e59d5a43cfd572
bncwhite <brad@codebybrad.com>
2014-10-10 14:52:06 -0500

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,50 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    int doubled = [number intValue] * 2;
+    
+    NSNumber *doubledNumber = [NSNumber numberWithInt:doubled];
+    
+    return doubledNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *numberedArray = [@[] mutableCopy];
+    
+    int valueA;
+    int valueB;
+    
+    if (number < otherNumber) {
+        valueA = (int)number;
+        valueB = (int)otherNumber;
+    } else {
+        valueA = (int)otherNumber;
+        valueB = (int)number;
+    }
+    
+    while (valueA <= valueB) {
+        NSNumber *number = [NSNumber numberWithInt:valueA];
+        [numberedArray addObject:number];
+        valueA++;
+    }
+    
+    return numberedArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSMutableArray *sortedArray = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *findLowestNumber = [[NSSortDescriptor alloc] initWithKey:nil ascending:true];
+    [sortedArray sortUsingDescriptors:@[findLowestNumber]];
+    
+    NSNumber *lowestNumber = sortedArray[0];
+    NSInteger lowest = [lowestNumber integerValue];
+    
+    return lowest;
 }
 
 @end
